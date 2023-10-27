6a67c31f7db6b020d99556304ee8eec6c0dd3f6e
Alessandro Souza <alessandrojsouza@gmail.com>
2023-10-27 05:55:24 -0300

    add delee product

--- src/app/views/products/products.component.ts
+++ src/app/views/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product } from 'src/app/models/product';
@@ -8,23 +8,22 @@ import { Product } from 'src/app/models/product';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
-  lista :  Product[] = [];
+  lista :  Product[] = [
+    {id:1,name:'Caneta',price:100,stock:10},
+    {id:2,name:'Livro',price:200,stock:20},
+    {id:3,name:'Caderno',price:300,stock:30},
+  ];
+
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       let id = params.get('id');
       if(id != null){
-        this.lista = this.lista.filter(item => console.log(item.id != Number(id)));
+        this.lista = this.lista.filter(item => item.id !== Number(id));
       }
-      else
-        this.lista = [
-          {id:1,name:'Caneta',price:100,stock:10},
-          {id:2,name:'Livro',price:200,stock:20},
-          {id:3,name:'Caderno',price:300,stock:30},
-        ];
     })
   }
 
