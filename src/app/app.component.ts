340cc66ec0b5f394fc29219b08efde2be412c96f
Alessandro Souza <alessandrojsouza@gmail.com>
2023-11-07 06:13:23 -0300

    código inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-learn';
+  usuario = {nome:'', email:''};
+
+  onSubmit(form : NgForm){
+    console.log(form);
+    console.log(this.usuario);
+  }
 }
